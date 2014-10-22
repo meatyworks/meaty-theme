42f20573f96b6dbda061ba3c9809b54d46b1c0f9
meatyworks <melrachocan@gmail.com>
2014-10-22 15:21:52 -0400

    index updated

--- base/index.php
+++ base/index.php
@@ -6,20 +6,22 @@
 get_header(); ?>
 
   <div class="container">  
-  			
+  						
+  	<div class="col-md-12">
+	<img class="steak" src="<?php echo get_template_directory_uri(); ?>/img/meatyfinal.svg" alt="Meaty Works Digital Studio">
+ 	</div> 
+
 	<?php if (have_posts()) : ?>	 			
   	<?php $home = new WP_Query('tag=home&order=ASC'); ?>
 	<?php while ($home->have_posts()) : $home->the_post(); ?>
-			
-  	<div class="col-md-12">
-<!--<?php the_content(); ?>-->
-	<img class="steak" src="<?php echo get_template_directory_uri(); ?>/img/meatyfinal.svg" alt="Meaty Works Digital Studio">
-<!-- 
- 	<h1 class="title">Meaty Works</h1>
- -->
- 	</div> 
 
-  	 <?php endwhile; ?>
+  	<div class="col-md-12">
+
+	<?php the_content(); ?>  	 
+
+	</div>
+	
+	<?php endwhile; ?>
 	 <?php endif; ?>
 
 
