302b0190def07fdef4f08f5cd6ff7804158e3dd1
meatyworks <melrachocan@gmail.com>
2014-10-23 16:27:04 -0400

    Oct 23

--- base/assets/js/main.js
+++ base/assets/js/main.js
@@ -1,5 +1,31 @@
 jQuery(document).ready(function($){ 
 
+$('img.steak').click(function(){
+	$('div.about-meaty').fadeIn();
+});
+
+$('a.team').click(function(){
+	$('div.team-meaty').fadeIn();
+});
+
+$('a.services').click(function(){
+	$('div.services-meaty').fadeIn();
+});
+
+//Timer Function Below
+/*
+$('#nav-helper a').click(function(){
+		setTimeout(function(){ 
+			$(".numbers").each(function(i) {
+				$(this).delay((i + 1) * 50).fadeIn();
+       		}); 
+			}, 1500
+		);				
+		return false;
+	});
+*/
+
+//SMOOTH SCROLL care of CSS TRICKS 
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
