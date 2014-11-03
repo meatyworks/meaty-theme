2f7cadd0f8139e97c6427b82ee4daffc7cdcd094
meatyworks <melrachocan@gmail.com>
2014-11-03 10:46:53 -0500

    merge Nov 3

---
+++ base/page-contact.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+* Template Name: Contact 
+**/
+?> 	
+ 	
+        <div class="col-md-12 content contact-meaty">
+
+        <h3 id="contact-meaty">Drop us a line!</h3>
+        <p>We'd love to discuss your meatiest ideas</p>
+        <p><a href="mailto:info@meatyworks.com?Subject=Meaty%20Inquiries">info@meatyworks.com</a></p>
+        </div>
